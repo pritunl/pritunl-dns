b36ecfa75ee21658b150e2d38fcf6cef3154e0ee
Zachary Huff <zach.huff.386@gmail.com>
2015-07-13 15:29:16 -0400

    Add networks

---
+++ networks/networks.go
@@ -0,0 +1,40 @@
+package networks
+
+import (
+	"net"
+	"github.com/dropbox/godropbox/errors"
+)
+
+var Networks = map[string]*net.IPNet{}
+
+func Update() (err error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		err = &SystemError{
+			errors.Wrap(err, "networks: Error getting interfaces"),
+		}
+		return
+	}
+
+	networks := map[string]*net.IPNet{}
+
+	for _, iface := range ifaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+
+		for _, addr := range addrs {
+			_, subnet, err := net.ParseCIDR(addr.String())
+			if err != nil {
+				continue
+			}
+
+			networks[subnet.String()] = subnet
+		}
+	}
+
+	Networks = networks
+
+	return
+}
