8178e7db9a7e587daf3074708f3ae59cf45847a4
Zachary Huff <zach.huff.386@gmail.com>
2015-07-13 16:18:23 -0400

    Fix formatting in handler

--- handler/handler.go
+++ handler/handler.go
@@ -56,7 +56,7 @@ func NewHandler(timeout, interval time.Duration) (h *Handler) {
 		reslvr: &resolver.Resolver{
 			Timeout:  timeout,
 			Interval: interval,
-			Servers: []string{"8.8.8.8:53", "8.8.4.4:53"},
+			Servers:  []string{"8.8.8.8:53", "8.8.4.4:53"},
 		},
 	}
 
