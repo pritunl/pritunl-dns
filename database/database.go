05a98b30cddd4957b1ebd4e1badb4a262b3d666a
Zachary Huff <zach.huff.386@gmail.com>
2016-03-08 02:10:18 -0500

    Fix database selection

--- database/database.go
+++ database/database.go
@@ -12,7 +12,6 @@ import (
 	"net/url"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 )
 
@@ -129,15 +128,7 @@ func Connect() (err error) {
 
 func GetDatabase() (db *Database) {
 	session := Session.Copy()
-
-	var dbName string
-	if x := strings.LastIndex(mongoUrl, "/"); x != -1 {
-		dbName = mongoUrl[x+1:]
-	} else {
-		dbName = "pritunl"
-	}
-
-	database := session.DB(dbName)
+	database := session.DB("")
 
 	db = &Database{
 		session:  session,
