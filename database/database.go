c3c7d0a0695ff0549f1f8515d8015154cfdd872f
Zachary Huff <zach.huff.386@gmail.com>
2020-09-09 11:46:02 -0400

    Update logrus import path

--- database/database.go
+++ database/database.go
@@ -3,7 +3,7 @@ package database
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"github.com/Sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/dropbox/godropbox/errors"
 	"github.com/pritunl/pritunl-dns/constants"
 	"gopkg.in/mgo.v2"
